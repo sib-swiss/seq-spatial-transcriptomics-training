d9a944d9ba37f92f6ed300898765a2be507ec5a9
Geert van Geest <geert.vangeest@bioinformatics.unibe.ch>
2025-10-29 17:10:51 +0100

    updates default to not mount to home dir

--- Docker/run_locally.sh
+++ Docker/run_locally.sh
@@ -1,5 +1,5 @@
 docker run \
 --rm \
 -p 8787:8787 \
--v $PWD:/home/rstudio \
+-v $PWD:/home/rstudio/seq-spatial-transcriptomics-training \
 sibswiss/training-seq-spatial-transcriptomics-rstudio:latest
